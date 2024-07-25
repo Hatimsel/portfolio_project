0c426e98c2fcaa0bd18301da5a8d749026099812
Wafaa HIJARY <95526772+Wafwaf14@users.noreply.github.com>
2024-07-25 18:28:54 +0100

    Delete Backend directory

--- Backend/routers/partnershipRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const partnershipController = require('../controllers/partnershipController');
-
-router.post('/partnership', partnershipController.submitRequest);
-
-module.exports = router;
--- Backend/routers/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userController = require('../controllers/userController');
-
-router.post('/api',userController.createUser);
-
-router.post('/login', userController.loginUser);
-
-module.exports = router;
