fded372dd06b3d705c7953252553ee92e9a71f4d
Hatim Selmun <selmun13@gmail.com>
2024-07-28 17:43:10 +0100

    CLeaning up: Moving all the database to the chatengine api

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,10 @@
 import express from "express";
 import cors from "cors";
-import axios from "axios";
 import dotenv from "dotenv";
-import connectDB from './utils/db.js';
-import userRouter from './routes/users.js';
 import authRoutes from "./routes/auth.js";
 import OpenAI from "openai";
 
 dotenv.config();
-
-connectDB();
 
 // GLOBAL CONFIGURATIONS
 const app = express();
@@ -22,25 +17,7 @@ export const openai = new OpenAI({
 });
 
 // ROUTES 
-app.use('/users', userRouter);
 app.use("/auth", authRoutes);
-
-app.post("/authenticate", async (req, res) => {
-  const { username, password } = req.body;
-  
-  try {
-    const r = await axios.put(
-      "https://api.chatengine.io/users/",
-      { username: username, secret: password, first_name: username },
-      { headers: { "private-key": process.env.PRIVATE_KEY }}
-    )
-
-    return res.status(r.status).json(r.data);
-  } catch(err) {
-    console.log(err);
-    return res.status(500).json(err);
-  }
-});
 
 // SERVER CONFIGURATION
 const port = process.env.PORT || 9000;
