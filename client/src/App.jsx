b9d4e0aa8031ee65b18fa7c21f00269e480448a3
Wafaa HIJARY <wafaa.elhijary@gmail.com>
2024-07-29 18:33:48 +0100

    last modif

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,42 @@
 import { useState } from "react";
-
-import "./App.css";
-
-import AuthPage from "./AuthPage";
-import ChatsPage from "./ChatsPage";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Chat from "@/components/chat";
+import Login from "@/components/login";
 
 function App() {
-  const [user, setUser] = useState(undefined);
+  const [user, setUser] = useState(null);
+  const [secret, setSecret] = useState(null);
+  const isAuth = Boolean(user) && Boolean(secret);
 
-  if (!user) {
-    return <AuthPage onAuth={(user) => setUser(user)} />;
-  } else {
-    return <ChatsPage user={user} />;
-  }
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isAuth ? (
+
+                <Navigate to="/chat" />
+              ) : (
+                <Login setUser={setUser} setSecret={setSecret} />
+              )
+            }
+          />
+          <Route
+            path="/chat"
+            element={
+              isAuth ? (
+                <Chat user={user} secret={secret} />
+              ) : (
+                <Navigate to="/" />
+              )
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
